b21a2766c6aff9e5687637d241f92d2fe26bc255
xuao <306536959@qq.com>
2025-07-16 17:38:30 +0800

    持续优化

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,33 +14,53 @@ import org.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.example.demo.mapper")
 public class DemoApplication extends SpringBootServletInitializer {
 
-    /**
-     * 配置 SpringApplicationBuilder
-     * @param builder 构建器实例
-     * @return 配置完成的构建器
-     */
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(DemoApplication.class);
-    }
-
     static {
-        try { Class.forName("com.ibm.db2.jcc.DB2Driver"); } catch (Exception ignored) {}
-        try { Class.forName("com.mysql.cj.jdbc.Driver"); } catch (Exception ignored) {}
-        try { Class.forName("org.opengauss.Driver"); } catch (Exception ignored) {}
-        try { Class.forName("oracle.jdbc.OracleDriver"); } catch (Exception ignored) {}
-        try { Class.forName("org.postgresql.Driver"); } catch (Exception ignored) {}
-        try { Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); } catch (Exception ignored) {}
-        try { Class.forName("dm.jdbc.driver.DmDriver"); } catch (Exception ignored) {}
-        try { Class.forName("com.oceanbase.jdbc.Driver"); } catch (Exception ignored) {}
+        try {
+            Class.forName("com.ibm.db2.jcc.DB2Driver");
+        } catch (Exception ignored) {
+        }
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception ignored) {
+        }
+        try {
+            Class.forName("org.opengauss.Driver");
+        } catch (Exception ignored) {
+        }
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+        } catch (Exception ignored) {
+        }
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (Exception ignored) {
+        }
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (Exception ignored) {
+        }
+        try {
+            Class.forName("dm.jdbc.driver.DmDriver");
+        } catch (Exception ignored) {
+        }
+        try {
+            Class.forName("com.oceanbase.jdbc.Driver");
+        } catch (Exception ignored) {
+        }
         System.out.println("所有主流数据库驱动已通过静态代码块强制注册");
     }
 
     /**
      * 应用主入口
+     *
      * @param args 启动参数
      */
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
     }
 }
